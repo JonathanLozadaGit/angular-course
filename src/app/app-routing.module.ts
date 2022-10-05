5b4fafb3cac795484c03749b44c8fbb010e1944a
Jonathan <si.jonathanlozada@gmail.com>
2022-10-05 14:13:02 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
+  {path: '', loadChildren: () => import('./pokemon/pokemon.module').then(m => m.PokemonModule)},
   {path: 'star-wars', loadChildren: () => import('./star-wars/star-wars.module').then(m => m.StarWarsModule)},
   {path: 'rick-and-morty', loadChildren: () => import('./rick-and-morty/rick-and-morty.module').then(m => m.RickAndMortyModule)},
   {path: 'pokemon', loadChildren: () => import('./pokemon/pokemon.module').then(m => m.PokemonModule)},
   {path: 'pokemon/:id', loadChildren: () => import('./pokemon/pokemon-detail.module').then(m => m.PokemonDetailModule)},
   {path: 'contact-form', loadChildren: () => import('./contact-form/contact-form.module').then(m => m.ContactFormModule)},
-  {path: '**', loadChildren: () => import('./pokemon/pokemon.module').then(m => m.PokemonModule)},
 ];
 
 @NgModule({
