603bc539bd8640d93a21cf20418d812a4e52002c
Jonathan <si.jonathanlozada@gmail.com>
2022-10-08 00:55:18 +0200

    fix

--- src/app/main/component/card/card.component.spec.ts
+++ src/app/main/component/card/card.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
@@ -9,7 +9,7 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports : [RouterTestingModule.withRoutes([])],
+      imports : [RouterModule],
       declarations: [ CardComponent ],
     })
     .compileComponents();
